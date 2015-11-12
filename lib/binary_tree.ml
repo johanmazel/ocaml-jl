ad270b5eb7517c83e62d3389ae9a0306f35cd11d
Johan Mazel <jmazel@johan-precision.r.nii.ac.jp>
2015-11-12 16:45:38 +0900

    Added binary_tree

---
+++ lib/binary_tree.ml
@@ -0,0 +1,30 @@
+
+open Printf
+
+type ('a, 'b) t =
+| Leaf of 'b
+| Node of 'a * ('a, 'b) t * ('a, 'b) t
+with compare, sexp, bin_io
+
+let rec fold
+    f_leaf
+    f_node
+    t
+    =
+  match t with 
+  | Leaf b -> f_leaf b
+  | Node (a, t1, t2) -> 
+    f_node
+      a 
+      (fold f_leaf f_node t1)
+      (fold f_leaf f_node t2)
+
+let to_string
+    to_string_leaf
+    to_string_node
+    t
+    =
+  fold
+    to_string_leaf
+    to_string_node    
+    t

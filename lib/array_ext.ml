e72d74dfac864e296936544d54366e48b5a8ff27
Johan Mazel <jmazel@johan-precision.r.nii.ac.jp>
2015-12-18 13:20:56 +0900

    Added Array_ext.shuffle

--- lib/array_ext.ml
+++ lib/array_ext.ml
@@ -24,3 +24,14 @@ let to_string
     list;
   
   Batteries.IO.close_out innerIO_output
+
+let shuffle a =
+  (* Fisher–Yates shuffle *)
+  for i = pred (Array.length a) downto 1 do
+    let j = Random.int (succ i) in
+    if i <> j (* faster to omit this test with arrays of about 100000 elements or more *) then (
+      let tmp = Array.unsafe_get a i in
+      Array.unsafe_set a i (Array.unsafe_get a j);
+      Array.unsafe_set a j tmp
+    )
+  done

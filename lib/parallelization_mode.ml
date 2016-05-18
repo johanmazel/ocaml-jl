7b143b7de8814e79632deb7bae61d9a2cf96f7f6
Johan Mazel <jmazel@johan-precision.r.nii.ac.jp>
2016-05-19 06:48:57 +0900

    typo in parallelization_mode.ml

--- lib/parallelization_mode.ml
+++ lib/parallelization_mode.ml
@@ -8,5 +8,5 @@ type t =
 let to_string parallelization_mode =
   (match parallelization_mode with
    | No_parallelization -> "No_parallelization"
-   | Parmap (number_of_cores, chunk_size) -> (sprintf "Parmap with %d cores and a chank size of %d" number_of_cores chunk_size)
+   | Parmap (number_of_cores, chunk_size) -> (sprintf "Parmap with %d cores and a chunk size of %d" number_of_cores chunk_size)
   )
